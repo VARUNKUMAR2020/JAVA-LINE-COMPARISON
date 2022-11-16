d4c3b92b8a53b6c6fb225c54cd458461fe13d0a2
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-16 13:46:42 +0530

    Add: To check Lines are equal or not

--- LineComparison.java
+++ LineComparison.java
@@ -25,12 +25,38 @@ public class LineComparison {
 
         System.out.println("The first co-ordinate is (" + X1 + "," + Y1 + ") ");
         System.out.println("The second co-ordinate is (" + X2 + "," + Y2 + ") ");
-        double length;
 
         // Formula for calculating the lenght of the co-ordinates:
-        int a = (X2 - X1) * (X2 - X1);
-        int b = (Y2 - Y1) * (Y2 - Y1);
-        length = Math.sqrt(a + b);
-        System.out.println("length of the line is " + length);
+        int x = (X2 - X1) * (X2 - X1);
+        int y = (Y2 - Y1) * (Y2 - Y1);
+        double lengthxy = Math.sqrt(x + y);
+        System.out.println("length of the line XY is " + lengthxy);
+
+        // Displaying message for entering the number:
+        System.out.println("Enter the first co-ordinate");
+
+        // inputs are stored in the variables
+        int A1 = input.nextInt();
+        int B1 = input.nextInt();
+
+        // Displaying message for entering the number:
+        System.out.println("Enter the second co-ordinate");
+
+        // inputs are stored in the variables
+        int A2 = input.nextInt();
+        int B2 = input.nextInt();
+
+        System.out.println("The first co-ordinate is (" + A1 + "," + B1 + ") ");
+        System.out.println("The second co-ordinate is (" + A2 + "," + B2 + ") ");
+        double lengthAB;
+
+        // Formula for calculating the lenght of the co-ordinates:
+        int a = (A2 - A1) * (A2 - A1);
+        int b = (B2 - B1) * (B2 - B1);
+        double lengthab = Math.sqrt(a + b);
+        System.out.println("length of the line AB is " + lengthab);
+
+        System.out.println((lengthab == lengthxy) ? "Lines are Equal" : "Lines are not equal");
+
     }
 }
